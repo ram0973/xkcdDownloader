3f4fcef9f76100c9c85e6841f5b8835e2836c42c
vasilewvv <vasilewvv@gmail.com>
2018-04-29 12:57:14 +0300

    first

---
+++ xkcd.tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("xkcd.tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("xkcd.tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7acdfb35-8740-463a-8da3-bf6f4e14267f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ xkcd.tests/xkcdDownloadTests.cs
@@ -0,0 +1,45 @@
+﻿using Flurl.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SubstringExtension;
+using System.Linq;
+using System.Net;
+
+namespace xkcd.tests
+{
+    [TestClass]
+    public class xkcdDownloadTests
+    {
+        [TestMethod]
+        public void Main()
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                var link = getPictureLink(i);
+                save(i, link);
+            }
+        }
+
+        private string getPictureLink(int num)
+        {
+            var source = $"https://xkcd.com/{num}/".GetStringAsync().Result;
+            var imageLink = source.Substring("imgs.xkcd.com/", "\"");
+            var link = $"http://imgs.xkcd.com/{imageLink}";
+            return link;
+        }
+
+        private void save(int i, string link)
+        {
+            var name = getName(link);
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(link, name);
+            }
+        }
+
+        private string getName(string link)
+        {
+            return link.Split('/').Last();
+        }
+
+    }
+}
